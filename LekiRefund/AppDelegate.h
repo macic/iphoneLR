befbdbf08d281a55b1976e32af613e6e5b42214c
macic <maciczka@gmail.com>
2012-02-26 23:21:09 +0100

    Initial Commit

---
+++ LekiRefund/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  LekiRefund
+//
+//  Created by dzidzi on 26.02.2012.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
